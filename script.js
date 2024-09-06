d1a5d9e5fedaff71ed69e6a3bee69476f045dc42
Piyush Kalsariya <114228003+code-piyush@users.noreply.github.com>
2024-09-06 16:15:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
   document.addEventListener("DOMContentLoaded", () => {
     document.cookie = "variationID=1004241571"; 
-    localStorage.setItem("variationID", "1004241571");
+    // localStorage.setItem("variationID", "1004241571");
   })
 })();
