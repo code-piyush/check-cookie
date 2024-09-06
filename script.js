7bf05cd7daf370d32cec2b80d961261b61e93645
Piyush Kalsariya <114228003+code-piyush@users.noreply.github.com>
2024-09-06 18:18:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
     document.cookie = "variationID=1004241571"; 
     // localStorage.setItem("variationID", "1004241571");
 
-    document.querySelector('.emailInput').style.display = 'none';
+    document.querySelectorAll('.emailInput')?.forEach((ele) => {
+      ele.style.display = 'none';
+    });
   })
 })();
