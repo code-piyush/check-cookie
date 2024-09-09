d2aeecece98ca8bff2327581498647b00b120da0
Piyush Kalsariya <114228003+code-piyush@users.noreply.github.com>
2024-09-09 09:59:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 (function () {
-  window.variationID = '1004241571'
     
-  // document.addEventListener("DOMContentLoaded", () => {
-  //   document.cookie = "variationID=1004241571"; 
-  //   // localStorage.setItem("variationID", "1004241571");
+  document.addEventListener("DOMContentLoaded", () => {
+    // document.cookie = "variationID=1004241571"; 
+    window.variationID = '1004241571'
+    // localStorage.setItem("variationID", "1004241571");
 
-  // })
     document.querySelectorAll('.emailInput')?.forEach((ele) => {
       ele.style.display = 'none';
     });
+  })
 })();
