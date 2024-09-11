3407303c2747c4f48c7ee0361798194c2d48d8f6
Piyush Kalsariya <114228003+code-piyush@users.noreply.github.com>
2024-09-11 14:53:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 (function () {
   document.addEventListener("DOMContentLoaded", () => {
-    window.variationID = '1004241571';
+    // window.variationID = '1004241571';
+    window.variationID = '1004241570';
     window.experienceID = '1004100739';
 
     document.querySelectorAll('.emailInput')?.forEach((ele) => {
