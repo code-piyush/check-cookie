056794846566e0feed108e4cfa44630e261bcd1d
Piyush Kalsariya <114228003+code-piyush@users.noreply.github.com>
2024-09-11 15:11:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
     window.variationID = '1004241570';
     window.experienceID = '1004100739';
 
-    document.querySelectorAll('.emailInput')?.forEach((ele) => {
-      ele.style.display = 'none';
-    });
+    // document.querySelectorAll('.emailInput')?.forEach((ele) => {
+    //   ele.style.display = 'none';
+    // });
   });
 })();
